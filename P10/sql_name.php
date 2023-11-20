753cada3a5a0f3764c034ca898ca383a91c216db
ARAA48 <151500525+ARAA48@users.noreply.github.com>
2023-11-20 14:26:48 +0800

    Add files via upload

---
+++ P10/sql_name.php
@@ -0,0 +1,12 @@
+<?php
+
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "2101020048_NI KADEK MUTIARA SETIAWATI";
+
+    $con = mysqli_connect($server,$user,$password,$database);
+
+    
+    
+?>
